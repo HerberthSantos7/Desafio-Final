79005f22f8317710f9e1cdc149275cf13ee22ada
HerberthSantos7 <herberthfelipe1234@gmail.com>
2024-11-22 15:40:16 -0300

    Commit Inicial

---
+++ src/pages/inicio/inicio.jsx
@@ -0,0 +1,12 @@
+import s from '../inicio/inicio.module.scss'
+
+export default function Inicio() {
+    return (
+      <section>
+        <section className={s.banner}>
+          <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+        </section>  
+      </section>      
+    );
+  }
+  
